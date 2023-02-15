828c5193025d3c8773deade33f415fe92630b520
Сергей Катаев <nimnul010@yandex.ru>
2023-02-15 09:22:18 +0300

    send_message()

--- homework.py
+++ homework.py
@@ -1,11 +1,20 @@
-...
+from http import HTTPStatus
+import json
+import logging
+import os
+import requests
+import telegram
+import time
+
+from telegram import Bot
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -24,7 +33,12 @@ def check_tokens():
 
 
 def send_message(bot, message):
-    ...
+    """Отправка сообщения в Telegram чат."""
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+    except telegram.error.TelegramError as error:
+        logging.error(f'Не удалось отправить сообщение в telegram: {error}')
+        raise Exception(error)
 
 
 def get_api_answer(timestamp):
